fdaec3114b4fe3970e27548328106cde5ff97264
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-17 00:49:01 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,29 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+ counter = 0
+ max = 0
+ while counter < array.length do
+  if max < array[counter]
+    max = array[counter]
+  end
+   counter += 1
+ end
+ 
+ return max
 end
 
 def find_min_value(array)
-  # Add your solution here
-end
+ counter = 0
+ min = 0
+ while counter < array.length do
+   if min > array[counter]
+     min = array[counter]
+   end
+   counter += 0
+ end
+ 
+ return min
+ end
